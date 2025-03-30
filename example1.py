ebfe37afd5723ebd9d01b7fe07bf4f7ce20103dc
myakoobi <myakoobi@gmu.edu>
2025-03-29 21:25:22 -0400

    example 1

---
+++ example1.py
@@ -0,0 +1,24 @@
+from neopixel import Neopixel
+import utime
+
+numpix = 60
+strip = Neopixel(numpix, 0, 0, "RGB")
+
+color = (0, 100, 100)  # Blue color
+
+delay = 0.05
+strip.brightness(42)
+
+while True:
+    # Light up each pixel one by one
+    for i in range(numpix):
+        strip.set_pixel(i, color)
+        strip.show()
+        utime.sleep(delay)
+    
+    # Turn off each pixel one by one
+    for i in range(numpix):
+        strip.set_pixel(i, (0, 0, 0))
+        strip.show()
+        utime.sleep(delay)
+
